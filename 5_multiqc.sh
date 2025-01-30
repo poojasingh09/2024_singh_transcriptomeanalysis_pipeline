6cbc8114e0498652ef465811d4e0d2cfc70a3ac7
p00ja <30841009+poojasingh09@users.noreply.github.com>
2025-01-30 15:26:42 +0100

    Update 5_multiqc.sh

--- 5_multiqc.sh
+++ 5_multiqc.sh
@@ -1,7 +1,6 @@
 #!bin/bash
 
 # samtool idxstats - .bam mapping statistics
-# March 2020
 ########### Change '-N' jobname, '-l h_vmem=' (storage), '-pe smp #' (number of cores)
 ########### samtools idxstats
 
