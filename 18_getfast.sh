2aae7364e2b471d9665c0746e7e74e95c5e76ca5
p00ja <30841009+poojasingh09@users.noreply.github.com>
2025-01-30 16:30:42 +0100

    Create 18_getfast.sh

---
+++ 18_getfast.sh
@@ -0,0 +1,1 @@
+gffread -w transcripts.fa -g /cl_tmp/singh_duenser/reference/O_niloticus_UMD_NMBU.dna.toplevel.fa /cl_tmp/singh_duenser/all_st26/gffcompare/gffread/filtered.gffread.gffcmp.all.annotated.gtf
